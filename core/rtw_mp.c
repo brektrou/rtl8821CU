9da655bb0dcd1add1da3431d715f4cdfbf60ecea
Philip Prindeville <philip.prindeville@windtalker.com>
2020-11-05 05:19:29 +0000

    issue #92: don't require floor() and ceil() functions
    Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>

--- core/rtw_mp.c
+++ core/rtw_mp.c
@@ -23,19 +23,16 @@
 	#include <rtw_bt_mp.h>
 #endif
 
-#ifdef CONFIG_MP_VHT_HW_TX_MODE
-#define CEILING_POS(X) ((X - (int)(X)) > 0 ? (int)(X + 1) : (int)(X))
-#define CEILING_NEG(X) ((X - (int)(X)) < 0 ? (int)(X - 1) : (int)(X))
-#define ceil(X) (((X) > 0) ? CEILING_POS(X) : CEILING_NEG(X))
 
-int rtfloor(float x)
-{
-	int i = x - 2;
-	while
-	(++i <= x - 1)
-		;
-	return i;
-}
+#ifdef CONFIG_MP_VHT_HW_TX_MODE
+#define CEILING_POS(X) (((X) - (int)(X)) != 0.0 ? (int)(X) + 1 : (int)(X))
+#define CEILING_NEG(X) ((int)(X))
+#define ceil(X) (((X) >= 0.0) ? CEILING_POS(X) : CEILING_NEG(X))
+
+#define FLOOR_POS(X) ((int)(X))
+#define FLOOR_NEG(X) (((X) - (int)(X)) != 0.0 ? (int)(X) - 1 : (int)(X))
+#define floor(X) (((X) >= 0.0) ? FLOOR_POS(X) : FLOOR_NEG(X))
+#define rtfloor(X) floor(X)
 #endif
 
 #ifdef CONFIG_MP_INCLUDED
