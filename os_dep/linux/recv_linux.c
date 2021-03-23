efd84db37dee63e8d093faa54db424584e501dd8
smp79 <sergey79@gmail.com>
2021-03-23 16:56:02 +0300

    Fix kernel 5.12 build

--- os_dep/linux/recv_linux.c
+++ os_dep/linux/recv_linux.c
@@ -355,8 +355,12 @@ static int napi_recv(_adapter *padapter, int budget)
 
 #ifdef CONFIG_RTW_GRO
 		if (pregistrypriv->en_gro) {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)	
 			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
 				rx_ok = _TRUE;
+#else
+			rx_ok = _TRUE;
+#endif
 			goto next;
 		}
 #endif /* CONFIG_RTW_GRO */
